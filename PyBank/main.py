2e4c1c5e642813402ef25777e46c29a711e6a1c8
Pournima07 <pournima@hotmail.com>
2019-09-08 14:47:39 -0400

    homework

---
+++ PyBank/main.py
@@ -0,0 +1,69 @@
+import os
+import csv
+
+csv_filename = 'C:\\Learn\\Python\\python Homework PayBank.csv'
+
+with open(csv_filename,newline='') as csv_file:
+
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    csv_header = next(csv_reader)
+    print("csv_header",csv_header)
+    print()
+
+    first_month_data = next(csv_reader)
+    first_month = first_month_data[0]
+    first_month_profit_loss = float(first_month_data[1])
+
+    list_of_months = []
+    monthly_changes = []
+    nbr_of_list_of_months = 1
+    net_profit_loss = first_month_profit_loss
+
+    best_monthly_change = 0
+    worst_monthly_change = 0
+    month_of_best_monthly_change = ""
+    month_of_worst_monthly_change = ""
+    prev_month_profit_loss = first_month_profit_loss
+
+    # Read second month onwards
+    for each_data in csv_reader:
+
+        nbr_of_list_of_months = nbr_of_list_of_months + 1
+        print("each_data", each_data)
+
+        current_month = each_data [0]
+        list_of_months.append( current_month )
+
+        current_month_profit_loss = float(each_data [1] )
+        net_profit_loss = net_profit_loss + current_month_profit_loss
+
+        change_current_month = current_month_profit_loss - prev_month_profit_loss
+        print("change in month: ", current_month, " was: ", change_current_month)
+        monthly_changes.append( change_current_month )
+
+        if change_current_month > best_monthly_change:  # Change this month is more than best so far
+
+            best_monthly_change = change_current_month
+            month_of_best_monthly_change = current_month
+            print('month_of_best_monthly_change: ', month_of_best_monthly_change)
+
+        elif change_current_month < worst_monthly_change: # Change this month is worse than the worst change so far
+
+            worst_monthly_change = change_current_month
+            month_of_worst_monthly_change = current_month
+            print('month_of_worst_monthly_change: ', month_of_worst_monthly_change )
+        
+        # For next time around the loop
+        prev_month_profit_loss = current_month_profit_loss
+
+        # For loop finished
+
+    # CSV Reader also is at the bottom of CSV file
+    print()
+    print("Total_numbers_of_list_of_months:", nbr_of_list_of_months)
+    print("Net_profit_loss:", round(net_profit_loss, 2))
+    ave_change = sum (monthly_changes) / len (monthly_changes)
+    print("Ave_change:", round(ave_change, 2))
+    print("greatest increase in profits:", round(best_monthly_change,2) , " in month ", month_of_best_monthly_change)
+    print("greatest decrease in losses:", round(worst_monthly_change, 2) , "in month", month_of_worst_monthly_change)
+
---
+++ PyPoll/main.py
@@ -0,0 +1,46 @@
+import os
+import csv
+csv_election_file= ('../election_data.csv')
+with open(csv_election_file,newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    csv_header = next(csv_reader)
+    #print("csv_header:",csv_header)
+    #print("Reading the CSV file ... ")
+    
+    num_of_votes = 0
+    votes_for_each_candidate = {}
+
+    for this_row in csv_reader:
+        
+        num_of_votes =num_of_votes + 1
+        this_candidate_name = this_row[2]
+
+        if this_candidate_name not in votes_for_each_candidate.keys():
+            votes_for_each_candidate [this_candidate_name] = 1
+        else:
+            votes_for_each_candidate [this_candidate_name] += 1
+            
+         
+
+# File got closed here
+# This line after with-clause
+
+max_votes = 0
+winner_name = ""
+percent_votes={}
+
+
+for candidate_name in votes_for_each_candidate.keys():
+    Vote_for_this_candidate = votes_for_each_candidate[candidate_name]
+    
+    if Vote_for_this_candidate > max_votes:
+        max_votes = Vote_for_this_candidate
+        winner_name = candidate_name       
+      
+    percent_votes[candidate_name] = str(round((votes_for_each_candidate[candidate_name] / num_of_votes)*100,2)) +"%"  
+
+print("Total Numbers of Votes:", num_of_votes)
+print("votes_for_each_candidate so far: ", votes_for_each_candidate)
+print("List of candidates: ", votes_for_each_candidate.keys())
+print("Percentage of Votes:", percent_votes)
+print("Winner is : ", winner_name)
